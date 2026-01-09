1176318a2a1de1f31d081884e7c849ba365352c3
YEnchev <ysenchev@gmail.com>
2026-01-09 16:10:43 +0200

    inin commit

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -1,2 +1,15 @@
 public class BarkingDog {
+    public static void main(String[] args) {
+
+        shouldWakeUp(false, 2);
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+
+        if (hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        }
+        return barking && (hourOfDay < 8 || hourOfDay > 22);
+
+    }
 }
