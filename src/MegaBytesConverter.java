e9df619d8e4a500f9da435b78c2586d314542891
YEnchev <ysenchev@gmail.com>
2026-01-09 15:16:11 +0200

    inin commit

--- src/MegaBytesConverter.java
+++ src/MegaBytesConverter.java
@@ -1,9 +1,19 @@
 public class MegaBytesConverter {
     public static void main(String[] args) {
 
+        printMegaBytesAndKiloBytes(-1024);
     }
 
     public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int remainingKilobytes = kiloBytes % 1024;
+            int megabytes = kiloBytes / 1024;
+
+            System.out.println(kiloBytes + " KB = " + megabytes + " MB and " + remainingKilobytes + " KB");
+        }
 
     }
 }
