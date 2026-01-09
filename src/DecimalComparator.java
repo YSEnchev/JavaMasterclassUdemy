524cbcd2e0bd38fc7f28ad7dc6d833d457246b31
YEnchev <ysenchev@gmail.com>
2026-01-10 00:15:28 +0200

    inin commit

--- src/DecimalComparator.java
+++ src/DecimalComparator.java
@@ -1,7 +1,15 @@
 public class DecimalComparator {
     public static void main(String[] args) {
 
+        areEqualByThreeDecimalPlaces(3.175, 3.176);
     }
 
+    public static boolean areEqualByThreeDecimalPlaces(double firstNumber, double secondNumber) {
+
+        long firstNum = (long) (firstNumber * 1000);
+        long secondNum = (long) (secondNumber * 1000);
+
+        return firstNum == secondNum;
+    }
 
 }
