638e6f9fd9af90306beb9e08e04d8e6baca740f5
YEnchev <ysenchev@gmail.com>
2026-01-12 23:43:54 +0200

    inin commit

--- src/SecondAndMinutesChallenge.java
+++ src/SecondAndMinutesChallenge.java
@@ -1,22 +1,36 @@
 public class SecondAndMinutesChallenge {
     public static void main(String[] args) {
+        System.out.println(getDurationString(-3945));
+        System.out.println(getDurationString(-65, 45));
+        System.out.println(getDurationString(65, 145));
         System.out.println(getDurationString(3945));
-        System.out.println(getDurationString(65, 45));
     }
 
     public static String getDurationString(int second) {
 
+        if (second < 0) {
+            return "Invalid data for second(" + second + "), must be a positive integer value";
+        }
+
         int minutes = second / 60;
-        return getDurationString(minutes, second);
+        return getDurationString(second / 60, second % 60);
     }
 
     public static String getDurationString(int minutes, int second) {
 
+        if (minutes < 0) {
+            return "Invalid data for second(" + minutes + "), must be a positive integer value";
+        }
+
+        if (second < 0 || second > 59) {
+            return "Invalid data for second(" + second + "), must be a between 0 and 59";
+        }
+
         int hours = minutes / 60;
 
         int remainingMinutes = minutes % 60;
-        int remainingSeconds = second % 60;
 
-        return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s";
+
+        return hours + "h " + remainingMinutes + "m " + second + "s";
     }
 }
