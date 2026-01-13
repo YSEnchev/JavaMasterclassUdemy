92825bad1c480c169f6fcef7fb8184bbec847ec0
YEnchev <ysenchev@gmail.com>
2026-01-13 23:13:52 +0200

    inin commit

---
+++ src/AreaCalculator.java
@@ -0,0 +1,22 @@
+public class AreaCalculator {
+    public static void main(String[] args) {
+
+        System.out.printf("%.5f%n", area(5.0, 4.0));
+    }
+
+    public static double area(double radius) {
+        if (radius < 0) {
+            return -1;
+        }
+
+        return Math.PI * radius * radius;
+    }
+
+    public static double area(double x, double y) {
+        if (x < 0 || y < 0) {
+            return -1;
+        }
+
+        return x * y;
+    }
+}
