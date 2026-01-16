d935bcdb8306168b7c3e7ae040e6d628e021954e
YEnchev <ysenchev@gmail.com>
2026-01-16 16:50:09 +0200

    inin commit

---
+++ src/PlayingCat.java
@@ -0,0 +1,12 @@
+public class PlayingCat {
+    public static void main(String[] args) {
+
+
+        isCatPlaying(true, 25);
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        int max = summer ? 45 : 35;
+        return temperature >= 25 && temperature <= max;
+    }
+}
