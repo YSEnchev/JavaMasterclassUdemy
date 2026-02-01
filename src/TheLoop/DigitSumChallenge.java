bf653d3e7c225e9076e199fbe5cffde57fd99782
YEnchev <ysenchev@gmail.com>
2026-02-01 23:33:29 +0200

    inin commit

---
+++ src/TheLoop/DigitSumChallenge.java
@@ -0,0 +1,31 @@
+package TheLoop;
+
+public class DigitSumChallenge {
+    public static void main(String[] args) {
+
+        System.out.println("The sum of the digits in number 1234 is " + sumDigits(1234));
+        System.out.println("The sum of the digits in number 1234 is " + sumDigits(-125));
+        System.out.println("The sum of the digits in number 1234 is " + sumDigits(4));
+        System.out.println("The sum of the digits in number 1234 is " + sumDigits(32123));
+
+    }
+
+    public static int sumDigits(int number) {
+
+        int sum = 0;
+
+        if (number < 0) {
+            return -1;
+        }
+
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        sum += number;
+
+        return sum;
+
+    }
+}
